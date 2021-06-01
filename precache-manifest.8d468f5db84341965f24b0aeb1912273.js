342b82fa17a1d582e583c72eae373bce83bb5502
aeddin <aeddin@i-doit.com>
2021-06-01 16:49:54 +0200

    Updates

---
+++ precache-manifest.8d468f5db84341965f24b0aeb1912273.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c869790dedfe452963a23635179d2414",
+    "url": "/Kawa/index.html"
+  },
+  {
+    "revision": "d524fbe69c3d2dd6bed8",
+    "url": "/Kawa/static/css/2.2a86faee.chunk.css"
+  },
+  {
+    "revision": "cb412ef37a1b3cf4fcc3",
+    "url": "/Kawa/static/css/main.8faecc3d.chunk.css"
+  },
+  {
+    "revision": "d524fbe69c3d2dd6bed8",
+    "url": "/Kawa/static/js/2.0add7c1b.chunk.js"
+  },
+  {
+    "revision": "5f20d62de7dbb7d49f55e8e804fa6018",
+    "url": "/Kawa/static/js/2.0add7c1b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cb412ef37a1b3cf4fcc3",
+    "url": "/Kawa/static/js/main.de8eebc1.chunk.js"
+  },
+  {
+    "revision": "65d6266474d97a06f5f4",
+    "url": "/Kawa/static/js/runtime-main.6c64ca44.js"
+  },
+  {
+    "revision": "1d5c9a3009241c25678c2858f829dd24",
+    "url": "/Kawa/static/media/kawa-1.1d5c9a30.png"
+  },
+  {
+    "revision": "c673ad860575cb4d825efcd958b052ef",
+    "url": "/Kawa/static/media/kawa-2.c673ad86.png"
+  },
+  {
+    "revision": "451d11d9805f3def1b829e6d3c728059",
+    "url": "/Kawa/static/media/kawa-home.451d11d9.png"
+  },
+  {
+    "revision": "5e63e1dc112289b47ef3405df58b30c5",
+    "url": "/Kawa/static/media/test2.5e63e1dc.png"
+  }
+]);
