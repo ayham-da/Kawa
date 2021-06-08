9ae86725da088356bac8f2e121a511dea6028e6c
aeddin <aeddin@i-doit.com>
2021-06-08 22:49:04 +0200

    ok

---
+++ build/precache-manifest.07c399e9416e4068137a257fd6f7aad8.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3936b23c534ae78de91b419c1c9d16af",
+    "url": "/Kawa/index.html"
+  },
+  {
+    "revision": "a929036cc11f268dc28c",
+    "url": "/Kawa/static/css/2.2a86faee.chunk.css"
+  },
+  {
+    "revision": "f5d8fa53280853294b51",
+    "url": "/Kawa/static/css/main.0ef3243e.chunk.css"
+  },
+  {
+    "revision": "a929036cc11f268dc28c",
+    "url": "/Kawa/static/js/2.52927c80.chunk.js"
+  },
+  {
+    "revision": "5f20d62de7dbb7d49f55e8e804fa6018",
+    "url": "/Kawa/static/js/2.52927c80.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f5d8fa53280853294b51",
+    "url": "/Kawa/static/js/main.8d5b2ea8.chunk.js"
+  },
+  {
+    "revision": "65d6266474d97a06f5f4",
+    "url": "/Kawa/static/js/runtime-main.6c64ca44.js"
+  },
+  {
+    "revision": "9152023229239cd3baf3343075ae2107",
+    "url": "/Kawa/static/media/Buddha.91520232.jpeg"
+  },
+  {
+    "revision": "1ea6758a261642fa46c754e3a583b366",
+    "url": "/Kawa/static/media/Cover me.1ea6758a.jpeg"
+  },
+  {
+    "revision": "1ea9ced142b124c742c075faafea5a18",
+    "url": "/Kawa/static/media/Kalam.1ea9ced1.jpeg"
+  },
+  {
+    "revision": "274ca990e8a6f9601c9012ec64884d63",
+    "url": "/Kawa/static/media/Once.274ca990.jpeg"
+  },
+  {
+    "revision": "11c53b8aa0c302d5d7fff4c9262607f3",
+    "url": "/Kawa/static/media/Roja Nû.11c53b8a.jpeg"
+  },
+  {
+    "revision": "a8b41a54120e76862e13a8d16753466d",
+    "url": "/Kawa/static/media/Take me.a8b41a54.jpeg"
+  },
+  {
+    "revision": "1c8c75305248edbe9185c0ec57103ba0",
+    "url": "/Kawa/static/media/Two souls from the sky.1c8c7530.jpeg"
+  },
+  {
+    "revision": "5c4c5bbc901d65f537629d20eb3ceddf",
+    "url": "/Kawa/static/media/beauty in the desert.5c4c5bbc.jpeg"
+  },
+  {
+    "revision": "2bd2a4620357d50d208f03c910b35ae0",
+    "url": "/Kawa/static/media/free thoughts.2bd2a462.jpeg"
+  },
+  {
+    "revision": "1d5c9a3009241c25678c2858f829dd24",
+    "url": "/Kawa/static/media/kawa-1.1d5c9a30.png"
+  },
+  {
+    "revision": "c673ad860575cb4d825efcd958b052ef",
+    "url": "/Kawa/static/media/kawa-2.c673ad86.png"
+  },
+  {
+    "revision": "451d11d9805f3def1b829e6d3c728059",
+    "url": "/Kawa/static/media/kawa-home.451d11d9.png"
+  },
+  {
+    "revision": "5e63e1dc112289b47ef3405df58b30c5",
+    "url": "/Kawa/static/media/test2.5e63e1dc.png"
+  },
+  {
+    "revision": "55c39c3ac0cc4267f91e2f43838ff0f8",
+    "url": "/Kawa/static/media/wosnek.55c39c3a.jpeg"
+  }
+]);
